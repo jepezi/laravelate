7a258d508156084fa71f7be03dbf43ad9221a770
Jirat Arinrith <jepezi@gmail.com>
2014-08-08 10:37:48 +0700

    route comonin, login and signup

--- app/routes.php
+++ app/routes.php
@@ -1,14 +1,23 @@
 <?php
 
-/*
-|--------------------------------------------------------------------------
-| Application Routes
-|--------------------------------------------------------------------------
-|
-| Here is where you can register all of the routes for an application.
-| It's a breeze. Simply tell Laravel the URIs it should respond to
-| and give it the Closure to execute when that URI is requested.
-|
-*/
 
-Route::get('/', array('as' => 'home', 'uses' => 'HomeController@index'));
+
+/*==========  Home  ==========*/
+Route::get( '/', 			['as' => 'home', 		'uses' => 'HomeController@index'] );
+
+Route::post('signup', 		['as' => 'signup', 		'uses' => 'AuthController@postSignup'] );
+
+/**
+ * Authentication
+ *
+ * Allow a user to log in and log out of the application
+ */
+// disble login page in favor of comeonin
+// Route::get('login',             ['uses' => 'SessionController@create',    'as' => 'session.create']); 
+Route::get('login/{provider}',  ['uses' => 'SessionController@authorise', 'as' => 'session.authorise']);
+Route::post('login',            ['uses' => 'SessionController@store',     'as' => 'session.store']);
+Route::get('logout',         ['uses' => 'SessionController@destroy',   'as' => 'session.destroy']);
+
+
+/*==========  Come on in (login or signup)  ==========*/
+Route::get('comeonin',		['as' => 'comeonin', 	'uses' => 'HomeController@comeonin'] );
