6272e65969a9365d0ccf7c41e37db7106345519a
Jirat Arinrith <jepezi@gmail.com>
2014-08-10 18:26:20 +0700

    model - user fillable property

--- app/models/User.php
+++ app/models/User.php
@@ -22,7 +22,7 @@ class User extends Eloquent implements UserInterface, RemindableInterface {
 	 * @var array
 	 */
 	protected $hidden = array('password', 'remember_token');
-	protected $fillable = array('email', 'password');
+	protected $fillable = array('email', 'password', 'first_name', 'last_name', 'message', 'website', 'twitter', 'gender');
 
 
 	/*==========  Auto-Hash password when creating new user  ==========*/
