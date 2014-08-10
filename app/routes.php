9d10e2e942ad8910d5c0e366d16f18368083ae2a
Jirat Arinrith <jepezi@gmail.com>
2014-08-10 18:20:15 +0700

    route - add account edit and complete routes

--- app/routes.php
+++ app/routes.php
@@ -7,6 +7,38 @@ Route::get( '/', 			['as' => 'home', 		'uses' => 'HomeController@index'] );
 
 Route::post('signup', 		['as' => 'signup', 		'uses' => 'AuthController@store'] );
 
+/*
+|--------------------------------------------------------------------------
+| Account
+|--------------------------------------------------------------------------
+| 
+| edit or do anything with account
+| 
+*/
+Route::group(['prefix' => 'account',	'before' => 'auth'], function()
+{
+
+        # Account Dashboard
+        Route::get('/', 		['as' => 'account.index', 			'uses' => 'AccountController@index'] );
+        
+        # Complete account after newly signup
+        Route::get('complete', 		['as' => 'account.complete', 			'uses' => 'AccountController@complete'] );
+        Route::put('complete', 		'AccountController@updateComplete' );
+
+        # Edit account
+        Route::get('edit', 		['as' => 'account.edit', 			'uses' => 'AccountController@edit'] );
+        Route::put('edit', 		'AccountController@update' );
+
+        # Change Password
+        // Route::get('change-password', array('as' => 'change-password', 'uses' => 'ChangePasswordController@getIndex'));
+        // Route::post('change-password', 'ChangePasswordController@postIndex');
+
+        # Change Email
+        // Route::get('change-email', array('as' => 'change-email', 'uses' => 'ChangeEmailController@getIndex'));
+        // Route::post('change-email', 'ChangeEmailController@postIndex');
+
+});
+
 /**
  * Authentication
  *
@@ -14,10 +46,10 @@ Route::post('signup', 		['as' => 'signup', 		'uses' => 'AuthController@store'] )
  */
 // disble login page in favor of comeonin
 // Route::get('login',             ['uses' => 'SessionController@create',    'as' => 'session.create']); 
-Route::get('login/{provider}',  ['uses' => 'SessionController@authorise', 'as' => 'session.authorise']);
-Route::post('login',            ['uses' => 'SessionController@store',     'as' => 'session.store']);
-Route::get('logout',         ['uses' => 'SessionController@destroy',   'as' => 'session.destroy']);
+Route::get('login/{provider}',  ['uses' => 'SessionController@authorise', 	'as' => 'session.authorise']);
+Route::post('login',            ['uses' => 'SessionController@store',     	'as' => 'session.store']);
+Route::get('logout',         	['uses' => 'SessionController@destroy',   	'as' => 'session.destroy']);
 
 
 /*==========  Come on in (login or signup)  ==========*/
-Route::get('comeonin',		['before' => 'guest', 'as' => 'comeonin', 	'uses' => 'HomeController@comeonin'] );
+Route::get('comeonin',			['before' => 'guest', 		'as' => 'comeonin', 	'uses' => 'HomeController@comeonin'] );
