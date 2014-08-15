b22156cf394004055c01ce020a9bac317c783e6d
Jirat Arinrith <jepezi@gmail.com>
2014-08-15 14:40:08 +0700

    Language files for activation and auth stuffs

---
+++ app/lang/en/activation.php
@@ -0,0 +1,22 @@
+<?php
+
+return array(
+
+	/*
+	|--------------------------------------------------------------------------
+	| Account Activation
+	|--------------------------------------------------------------------------
+	|
+	| The following language lines are the default lines for activation flow
+	| Used in several places. 
+	| - filters.php
+	| 
+	|
+	*/
+
+	"success" => "Your account has been activated! You can edit your account information anytime. Enjoy!",
+	"activated" => "Your account has been activated!",
+	"emailsubject" => "Please Activate Your Account!",
+	"fail" => 'There was an error. Please try activate your account again.',
+
+);
---
+++ app/lang/en/auth.php
@@ -0,0 +1,29 @@
+<?php
+
+return array(
+
+	/*
+	|--------------------------------------------------------------------------
+	| Auth
+	|--------------------------------------------------------------------------
+	|
+	| The following language lines are the default lines for auth flow including 
+	| login, register, authorise, ...
+	| Used in several places. 
+	| - filters.php
+	| 
+	|
+	*/
+
+	"signup" => array(
+		"success" => "Your account has been created! You can edit your account information anytime. Enjoy!"
+		),
+
+	"passwordreset" => array(
+		"emailsubject" => "Password Reset Instruction from Spring",
+		"success" => "Congrats! You can login with your new password."
+		),
+
+	"nativeuser" => "Your account has been connected with social provider. No need to change password."
+
+);
