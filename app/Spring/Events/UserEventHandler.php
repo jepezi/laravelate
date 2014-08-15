e649c451e4834abcf97d522a1f4cc07b3281437c
Jirat Arinrith <jepezi@gmail.com>
2014-08-15 14:41:55 +0700

    event handler - user - signed up and activate

--- app/Spring/Events/UserEventHandler.php
+++ app/Spring/Events/UserEventHandler.php
@@ -1,14 +1,21 @@
 <?php namespace Spring\Events;
 
-class UserEventHandler {
+class UserEventHandler 
+{
 
-  /**
-   * Send a welcome email to the new user
-   *
-   * @param User $user
-   * @return void
-   */
-  public function onUserCreated($user)
+  public function onUserActivate($user)
+  {
+    $user->activation_code = $this->getRandomString();
+    $user->save();
+
+    $data['user'] = $user;
+    \Mail::send('emails.activate', $data, function($message) use ($user)
+    {
+      $message->to($user->email, $user->first_name . ' ' . $user->last_name)->subject(\Lang::get('activation.emailsubject'));
+    });
+  }
+
+  public function onUserSignedUp($user)
   {
     $data['user'] = $user;
     \Mail::send('emails.welcome', $data, function($message) use ($user)
@@ -25,7 +32,42 @@ class UserEventHandler {
    */
   public function subscribe($events)
   {
-    $events->listen('user.created', 'Spring\Events\UserEventHandler@onUserCreated');
+    $events->listen('user.signedup', 'Spring\Events\UserEventHandler@onUserSignedUp');
+    $events->listen('user.activate', 'Spring\Events\UserEventHandler@onUserActivate');
+  }
+
+  /*
+  |--------------------------------------------------------------------------
+  | Helper function
+  |--------------------------------------------------------------------------
+  | 
+  | 
+  | 
+  */
+  public function getRandomString($length = 42)
+  {
+    // We'll check if the user has OpenSSL installed with PHP. If they do
+    // we'll use a better method of getting a random string. Otherwise, we'll
+    // fallback to a reasonably reliable method.
+    if (function_exists('openssl_random_pseudo_bytes'))
+    {
+      // We generate twice as many bytes here because we want to ensure we have
+      // enough after we base64 encode it to get the length we need because we
+      // take out the "/", "+", and "=" characters.
+      $bytes = openssl_random_pseudo_bytes($length * 2);
+
+      // We want to stop execution if the key fails because, well, that is bad.
+      if ($bytes === false)
+      {
+        throw new \RuntimeException('Unable to generate random string.');
+      }
+
+      return substr(str_replace(array('/', '+', '='), '', base64_encode($bytes)), 0, $length);
+    }
+
+    $pool = '0123456789abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ';
+
+    return substr(str_shuffle(str_repeat($pool, 5)), 0, $length);
   }
 
 }
